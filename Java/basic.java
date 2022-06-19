ae792844d593b3a932889410af1f899444943154
Navaneeth-krishna-30 <navaneethkrishnamudumba@gmail.com>
2022-06-19 17:52:38 +0530

    basics of java
    followed bro code from YouTube and started learning java

---
+++ Java/basic.java
@@ -0,0 +1,17 @@
+//this is a basic programe
+public class basic{
+    public static void main(String[] args){
+        System.out.println("Hallo");
+
+        int a=10;
+        double b=69.420;
+        char ch='l';
+        String str="Test String";
+
+        System.out.println("integer: " + a);
+        System.out.println("decimal: " + b);
+        System.out.println("char: " + ch);
+        System.out.println("String: " + str);
+    }
+}
+    
